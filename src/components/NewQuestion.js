ece580bf0aceff3d1540e2d422cf9026411ebd36
Abubaker Siddiq <abubakershekhani@gmail.com>
2019-10-24 15:54:08 +0500

    Add NewQuestion component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,23 +1,105 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleAddQuestion, handleGetQuestions } from "../actions/questions";
 
 class NewQuestion extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { optionOne: "", optionTwo: "" };
+  }
+
+  handleChange = (event, option) => {
+    if (option === "OptionOne") {
+      this.setState({optionOne: event.target.value})
+    } else {
+      this.setState({optionTwo: event.target.value})
+    }
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const question = {
+      optionOneText: this.state.optionOne,
+      optionTwoText: this.state.optionTwo,
+      author: this.props.authedUser
+    }
+
+
+    this.props.dispatch(handleAddQuestion(question))
+    this.props.history.push('/')
+
+
+
+  }
+
+
   render() {
-    return(
-      <div>
-        Post a Poll
+    const { authedUser } = this.props;
+
+    return (
+      <div className="ui container">
+        <div className="ui grid center aligned">
+          <div className="ui row center aligned">
+            <div className="eight wide column">
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>Post a Poll</h2>
+              <form className="ui large form" onSubmit={this.handleFormSubmit}>
+                <div className="ui stacked segment">
+                  <div className="field">
+                    <div class="ui left labeled input">
+                      <label for="optionOne" class="ui label">
+                        Option 1
+                      </label>
+                      <input
+                        type="text"
+                        name="optionOne"
+                        id="optionOne"
+                        maxLength="40"
+                        value={this.state.optionOne}
+                        onChange={(event) => this.handleChange(event, "OptionOne")}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div class="ui left labeled input">
+                      <label for="optionTwo" class="ui label">
+                        Option 2
+                      </label>
+                      <input
+                        type="text"
+                        name="optionTwo"
+                        id="optionTwo"
+                        maxLength="40"
+                        value={this.state.optionTwo}
+                        onChange={(event) => this.handleChange(event, "optionTwo")}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div class="ui left labeled input"></div>
+                    <button class="positive ui button">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-
-function mapStateToProps({users, authentication}) {
+function mapStateToProps({ users, authentication, questions }) {
   return {
-      users,
-      authenticated: authentication,
-  }
+    users,
+    questions,
+    authedUser: authentication
+  };
 }
 
-export default connect(mapStateToProps)(NewQuestion)
+export default connect(mapStateToProps)(NewQuestion);
