9a1bc846f26e44d9b31e244a4518e4eb5b44dad5
Abubaker Siddiq <abubakershekhani@gmail.com>
2019-10-25 10:19:17 +0500

    Refactor code and create sub components

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,103 +1,84 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import './App.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import "./App.css";
+import QuestionItem from "./QuestionItem";
 
 class QuestionList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { uaq: true, aq: false };
+  }
+
+  handleMenuChange = option => {
+    if (option === "UAQ") {
+      this.setState({ uaq: true, aq: false });
+    } else {
+      this.setState({ uaq: false, aq: true });
+    }
+  };
 
   render() {
-
-    const authedUser = this.props.authenticated
-    const { answeredQuestions, unAnsweredQuestions, users } = this.props
+    const authedUser = this.props.authenticated;
+    const { answeredQuestions, unAnsweredQuestions, users } = this.props;
 
     return (
-              <div className="ui container centered">
-                    <h2>Questions</h2>
-                    <h3>UnAnswered Questions</h3>
-                    { unAnsweredQuestions.map((question) =>
-                        /*
-                        <div class="row">
-                            <div>
-                                <div>{question.author} asks:</div>
-                                <div><h4>Would you rather</h4></div>
-                                <div></div>
-                                <div>Option 1: {question.optionOne.text} - Votes: {question.optionOne.votes.length}</div>
-                                <div>Option 2: {question.optionTwo.text} - Votes: {question.optionTwo.votes.length}</div>
-                                <div>Dated: {question.timestamp}</div>
-                                <div><button>View</button></div>
-                            </div>
-                            <hr />
-                        </div>
-                        */
+      <div>
+        <div className="ui compact menu">
+          <a className="item" onClick={event => this.handleMenuChange("UAQ")}>
+            UnAnswered Questions
+          </a>
+          <a className="item" onClick={event => this.handleMenuChange("AQ")}>
+            Answered Questions
+          </a>
+        </div>
 
-                       <div className="ui card" key={question.id}>
-                        <div className="content">
-                          <div className="right floated meta">14h</div>
-                          <img className="ui avatar image" src={users[question.author].avatarURL} alt={question.author}  /> {question.author} asks
-                        </div>
-                        <div className="image">
-                          <img />
-                        </div>
-                        <div className="content">
-                          <a className="header">Would You Rather?</a>
-                          <div className="meta">
-                          </div>
-                          <div className="description">
-                            <div>Option 1: {question.optionOne.text} - Votes: {question.optionOne.votes.length}</div>
-                            <div>Option 2: {question.optionTwo.text} - Votes: {question.optionTwo.votes.length}</div>
-                          </div>
-                        </div>
-                        <div className="extra content right aligned">
-                          <Link to={`/questions/${question.id}`}>
-                            View Poll
-                          </Link>
-                        </div>
-                       </div>
-                        ) }
-
-                    <h3>Answered Questions</h3>
-                    { answeredQuestions.map((question) =>
-
-                        <div className="ui card" key={question.id}>
-                        <div className="content">
-                          <div className="right floated meta">14h</div>
-                          <img className="ui avatar image" src={users[question.author].avatarURL} alt={question.author} /> {question.author} asks
-                        </div>
-                        <div className="image">
-                          <img />
-                        </div>
-                        <div className="content">
-                          <a className="header">Would You Rather?</a>
-                          <div className="meta">
-                          </div>
-                          <div className="description">
-                            <div>Option 1: {question.optionOne.text} - Votes: {question.optionOne.votes.length}</div>
-                            <div>Option 2: {question.optionTwo.text} - Votes: {question.optionTwo.votes.length}</div>
-                          </div>
-                        </div>
-                        <div className="extra content right aligned">
-                          <Link to={`/questions/${question.id}`}>
-                            View Poll
-                          </Link>
-                        </div>
-                       </div>
-                     ) }
+        <div className="ui container centered">
+          <h2>Questions</h2>
+          {this.state.uaq && (
+            <div>
+              <h3>UnAnswered Questions</h3>
+              {unAnsweredQuestions.map(question => (
+                <QuestionItem
+                  question={question}
+                  users={users}
+                  key={question.id}
+                />
+              ))}
             </div>
-          );
+          )}
+
+          {this.state.aq && (
+            <div>
+              <h3>Answered Questions</h3>
+              {answeredQuestions.map(question => (
+                <QuestionItem
+                  question={question}
+                  users={users}
+                  key={question.id}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
-function mapStateToProps({users, authentication, questions}) {
+function mapStateToProps({ users, authentication, questions }) {
   return {
-      users,
-      authenticated: authentication,
-      questions,
-      unAnsweredQuestions: Object.values(questions).filter(q =>
-          !users[authentication].answers.hasOwnProperty(q.id)).sort((a,b) => b.timestamp - a.timestamp),
-      answeredQuestions: Object.values(questions).filter(q =>
-          users[authentication].answers.hasOwnProperty(q.id)).sort((a,b) => b.timestamp - a.timestamp),
-
-  }
+    users,
+    authenticated: authentication,
+    questions,
+    unAnsweredQuestions: Object.values(questions)
+      .filter(q => !users[authentication].answers.hasOwnProperty(q.id))
+      .sort((a, b) => b.timestamp - a.timestamp),
+    answeredQuestions: Object.values(questions)
+      .filter(q => users[authentication].answers.hasOwnProperty(q.id))
+      .sort((a, b) => b.timestamp - a.timestamp)
+  };
 }
 
-export default connect(mapStateToProps)(QuestionList)
+export default connect(mapStateToProps)(QuestionList);
